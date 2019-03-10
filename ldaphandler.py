d15b452c9c90afe0d0a4a1a28b9d7e31fcb9a5cd
Daniel Stein <d.stein@hackherz.com>
2019-03-10 11:39:42 +0100

    Error handling for ldap

--- ldaphandler.py
+++ ldaphandler.py
@@ -5,19 +5,30 @@ import settings
 
 connect = ldap.initialize(settings.data['ldap']['server'])
 connect.set_option(ldap.OPT_REFERRALS, 0)
-connect.simple_bind_s(settings.data['ldap']['binddn'],
-        settings.data['ldap']['bindpw'])
 
+# Establish ldap connection
+def ldapConnect():
+    connect.simple_bind_s(settings.data['ldap']['binddn'],
+            settings.data['ldap']['bindpw'])
 
+# bind to ldap
+ldapConnect()
+
+# get dn of a user
 def getDn(uid):
     # Check if username is empty
     if uid == "":
         return False
 
     # first get the cn
-    query = settings.data['ldap']['userfilter'] % uid
-    ldap_user = connect.search_s(settings.data['ldap']['usertree'],
-            ldap.SCOPE_SUBTREE, query, ['cn'])
+    try:
+        query = settings.data['ldap']['userfilter'] % uid
+        ldap_user = connect.search_s(settings.data['ldap']['usertree'], 
+                ldap.SCOPE_SUBTREE, query, ['cn'])
+    except ldap.SERVER_DOWN:
+        print("exception")
+        ldapConnect()
+
 
     # User does not exist
     if len(ldap_user) == 0:
@@ -26,13 +37,20 @@ def getDn(uid):
     return ldap_user[0][0]
 
 
+# get groups of a user
 def getGroups(uid):
+
     if uid == "":
         return []
 
-    query = settings.data['ldap']['groupfilter'] % uid
-    ldap_groups = connect.search_s(settings.data['ldap']['grouptree'],
-            ldap.SCOPE_SUBTREE, query, ['cn'])
+    try:
+        query = settings.data['ldap']['groupfilter'] % uid
+        ldap_groups = connect.search_s(settings.data['ldap']['grouptree'],
+                ldap.SCOPE_SUBTREE, query, ['cn'])
+
+    except ldap.SERVER_DOWN:
+        print("exception")
+        ldapConnect()
 
     # Filter the relevant stuff
     buf = []
