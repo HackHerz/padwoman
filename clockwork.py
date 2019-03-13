f084843e55876c3bed1ccd9d94362ada68f35476
Daniel Stein <d.stein@hackherz.com>
2019-03-13 19:22:39 +0100

    Random Stuff is fun :)

--- clockwork.py
+++ clockwork.py
@@ -1,4 +1,5 @@
 import redis
+from random import shuffle
 
 # own stuff
 import settings
@@ -22,6 +23,9 @@ def updateTimestamps():
     
     # guessing which pads are not in the cache
     notCache = [ p for p in padList if p not in cList ]
+
+    # shuffle the list for a fair distribution
+    shuffle(notCache)
     
     # getting the timestamps
     for pad in notCache:
